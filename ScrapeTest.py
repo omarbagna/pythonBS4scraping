df5c570bab3ff80d96bd7999e4f997763b0a6f95
Bagna Farouk Omar <91009147+omarbagna@users.noreply.github.com>
2021-12-25 21:09:21 +0000

    Update ScrapeTest.py

--- ScrapeTest.py
+++ ScrapeTest.py
@@ -1,86 +1,180 @@
-from bs4 import BeautifulSoup
 import requests
-import xlsxwriter
+import random
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+from time import sleep
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0'}
+
+url = 'https://locations.wendys.com/united-states'
+
+def getData(url):
+    r = requests.get(url, headers=headers)
+    soup = bs(r.text, 'html.parser')
+    return soup
+
+
+WenStates = []
+
+pageItems = getData(url).find_all('li', {'class': 'Directory-listItem'})
+for item in pageItems:
+    states = {
+        'State': item.find('a', {'class': 'Directory-listLink'}).text,
+        'Link': 'https://locations.wendys.com/' + item.find('a', {'class': 'Directory-listLink'})['href']
+    }
+
+    WenStates.append(states)
 
 
 
-#collect html file content from website
-topMoviesChart = requests.get('https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm').text
+WenCities = []
 
-#activate beautiful soup to filter the data to be readable
-soup = BeautifulSoup(topMoviesChart, 'lxml')
+for WenState in WenStates:   
+    currentState = WenState['State']
+    stateSoup = getData(WenState['Link'])
 
-#target the area where the desired data is located
-chartContent = soup.find('tbody', class_='lister-list')
-
-#pull all rows containing the desired data
-chartRows = chartContent.find_all('tr')
-
-#create empty distionaries to hold the scraped data
-movies = []
-releaseYears = []
-viewerRatings = []
-
-row = 1
-col = 0
-
-
-#loop through all rows
-for chartRow in chartRows:
-    #target the title column
-    titles = chartRow.find_all('td', class_='titleColumn')
-    
-    #loop through all titles
-    for title in titles:
-        #pull the movie name from the title column
-        name = title.find('a').text
-
-        #pull the movie year from the title column
-        year = title.find('span', class_='secondaryInfo').text.strip("()")
+    pageItems = stateSoup.find_all('li', {'class': 'Directory-listItem'})
+    for item in pageItems:
         
-        #add the movie name to the movie dictionary
-        movies.append(name)
-
-        #add the movie year to the releaseYear dictionary
-        releaseYears.append(year)
+        State = currentState
+        City = item.find('a', {'class': 'Directory-listLink'}).text  
+        locations = int(item.find('span', {'class': 'Directory-listLinkCount'}).text.strip("()"))
         
-    #pull all the data in the ratings column
-    ratings = chartRow.find_all('td', class_='ratingColumn imdbRating')
+        if locations == 1:
+            initLink = item.find('a', {'class': 'Directory-listLink'})['href']
+            initLink = initLink[2 : ]
+            finalLink = 'https://locations.wendys.com' + initLink
+            subLink = ''   
+        else:
+            finalLink = ''
+            subLink = 'https://locations.wendys.com' + item.find('a', {'class': 'Directory-listLink'})['href'].replace('.','')
+            
+
+        cities = {
+            'State': State,
+            'City': City,
+            'Final-Link': finalLink,
+            'Sub-Link': subLink,
+        }
+
+        WenCities.append(cities)
     
-    #loop through the ratings data
-    for rating in ratings:
-       
-       #check if there is a rating
-       if rating.find('strong') not in rating:
-           #if there is no rating in the column
-           rate = "No rating"
-       else:
-           #if there is a rating in the column
-           rate = rating.find('strong').text
-
-        #add the rating to the viewerRating dictionary
-       viewerRatings.append(rate)
 
 
+Links = []
+print('Scraping Links....')
 
-# Create an new Excel file and add a worksheet.
-with xlsxwriter.Workbook('IMDB Scraped Data.xlsx') as workbook:
-    # Add worksheet
-    worksheet = workbook.add_worksheet('Top Movies')
+for WenCity in WenCities:
+    
+    
+    if WenCity['Final-Link'] != '':
+        finalLink = WenCity['Final-Link']
+        Links.append(finalLink)
+    elif WenCity['Sub-Link'] != '':
+        citySoup = getData(WenCity['Sub-Link'])
+        
+        pageItems = citySoup.find_all('div', {'class': 'Teaser-link'})
+        for item in pageItems:
+            initLink = item.find('a', {'class': 'Link'})['href']
+            initLink = initLink[5: ]
+            finalLink = 'https://locations.wendys.com' + initLink
+            Links.append(finalLink)
+            
 
-    # Write headers
-    worksheet.write(0, 0, 'Movie Title')
-    worksheet.write(0, 1, 'Year')
-    worksheet.write(0, 2, 'Rating')
 
-    # Write movie title data to excel file
-    for i, (movie) in enumerate(movies, start=1):
-        worksheet.write(i, 0, movie)
+#print(len(Links))
 
-    # Write movie year data to excel file
-    for i, (releaseYear) in enumerate(releaseYears, start=1):
-        worksheet.write(i, 1, releaseYear)
+print('Links Scraped Successfully')
 
-    # Write movie rating data to excel file
-    for i, (viewerRating) in enumerate(viewerRatings, start=1):
-        worksheet.write(i, 2, viewerRating)
+WenCitiesData = []
+
+print('Scraping Data....')
+
+count = 0
+
+for Link in Links:
+#    print(Link)
+    citySoup = getData(Link)
+    pageItems = citySoup.find('div', {'class': 'LocationInfo-content'})
+
+
+    stateExist = pageItems.find('abbr', {'class': 'c-address-state'})
+    if stateExist != None:
+        State = pageItems.find('abbr', {'class': 'c-address-state'})['title']
+    else:
+        State = 'Not Available'
+    
+    
+    cityExist = pageItems.find('span', {'class': 'c-address-city'})
+    if cityExist != None:
+        City = pageItems.find('span', {'class': 'c-address-city'}).text
+    else:
+        City = 'Not Available'
+    
+
+    zipExist = pageItems.find('span', {'class': 'c-address-postal-code'})
+    if zipExist != None:
+        Zip = pageItems.find('span', {'class': 'c-address-postal-code'}).text
+    else:
+        Zip = 'Not Available'
+
+    
+    streetExist = pageItems.find('span', {'class': 'c-address-street-1'})
+    if streetExist != None:
+        Street = pageItems.find('span', {'class': 'c-address-street-1'}).text
+    else:
+        Street = 'Not Available'
+    
+    
+    numExist = pageItems.find('span', {'class': 'c-phone-number-span c-phone-main-number-span'})
+    if numExist != None:
+        Number = pageItems.find('span', {'class': 'c-phone-number-span c-phone-main-number-span'}).text
+    else:
+        Number = 'Not Available'
+    
+    
+    locExist = citySoup.find('a', {'class': 'c-get-directions-button'})
+    if locExist != None:
+        Location = citySoup.find('a', {'class': 'c-get-directions-button'})['href']
+    else:
+        Location = 'Not Available'
+    
+
+    opened = pageItems.find('div', {'class': 'c-location-hours-details-wrapper js-location-hours'})
+    openHours = opened.find_all('tr', {'class': 'c-location-hours-details-row js-day-of-week-row highlight-text highlight-background'})
+
+    date = []
+
+    for openHour in openHours:
+        dateTime = openHour['content']
+
+
+        date.append(dateTime)
+
+
+    data = {
+        'State': State,
+        'City': City,
+        'Zip-Code': Zip,
+        'Street': Street,
+        'Number': Number,
+        'Location': Location,
+        'Open-Hours': date,
+    }
+    
+    count += 1
+    print(f'Scraped Data from Link: {count}')
+#    print(data)
+    WenCitiesData.append(data)
+    wait_time = random.randint(15, 45)
+    print(f'Waiting for {wait_time} seconds')
+    sleep(wait_time)
+
+
+print('Data Scraped Successfully')
+
+print(len(WenCitiesData))
+
+df = pd.DataFrame(WenCitiesData)
+df.to_csv('WendysData.csv', index=False)
+print('Successfully Scraped')
